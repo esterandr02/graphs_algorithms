d2667167e24f36b833e5bd26ef417a5fb1b29a9a
esterandr02 <esterandr02@gmail.com>
2021-09-24 23:14:36 -0300

    adjust run commands

--- README.md
+++ README.md
@@ -2,18 +2,21 @@
 Graphs algorithms by Ester Albuquerque and Juliana Melo - 2020.2 - Graphs Theory
 
 ### Algorithms
+
+- Before run, get into each algorithm folder
+
 - Bellman Ford:
   - To compile: make bellman_ford
-  - To execute: ./bellman <input.txt
+  - To execute: ./bellman_ford < input.txt
   
 - Dijkstra:
   - To compile: make dijkstra
-  - To execute: ./dijkstra <input.txt
+  - To execute: ./dijkstra < input.txt
   
 - Kruskal:
   - To compile: make kruskal
-  - To execute: ./kruskal <input.txt
+  - To execute: ./kruskal < input.txt
   
 - Prim:
   - To compile: make prim
-  - To execute: ./prim <input.txt
+  - To execute: ./prim < input.txt
--- bellman_ford/bellman_ford.cpp
+++ bellman_ford/bellman_ford.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
 using namespace std;
 
 typedef long long int lli;
-typedef pair <lli, lli> ii;
-typedef vector <lli> vi;
-typedef vector <ii> vii;
-typedef vector <vii> vvi;
+typedef pair<lli, lli> ii;
+typedef vector<lli> vi;
+typedef vector<ii> vii;
+typedef vector<vii> vvi;
 
 vvi graph;
 vi distances;
@@ -13,39 +16,92 @@ bool hasNegativeCycle = false;
 
 lli MAX_INT = 99999999;
 
-lli bellman_ford(int source) {
+lli bellman_ford(int source)
+{
   distances[source] = 0;
   lli vertex = graph.size();
   lli edge, cost;
 
-  for(int i=0; i <  vertex; i++) {
-    for(int j=0; j < vertex; j++) {
-      for(int k = 0; k < graph[j].size(); k++) {
+  for (int i = 0; i < vertex; i++)
+  {
+    for (int j = 0; j < vertex; j++)
+    {
+      for (int k = 0; k < graph[j].size(); k++)
+      {
         cost = graph[j][k].first;
         edge = graph[j][k].second;
 
-        if(i == vertex && distances[edge] > distances[j] + cost) hasNegativeCycle = true;
-        else distances[edge] = min(distances[edge], distances[j] + cost);
+        if (i == vertex && distances[edge] > distances[j] + cost)
+          hasNegativeCycle = true;
+        else
+          distances[edge] = min(distances[edge], distances[j] + cost);
       }
     }
   }
   return distances[vertex - 1];
 }
 
-int main() {
-  lli vertex, edges;
-  cin >> vertex >> edges;
+int main(int argc, char const *argv[])
+{
 
-  distances.resize(vertex + 1, MAX_INT);
-  graph.resize(vertex + 1);
+  // MENU
 
-  lli source, destiny, cost;
+  int i;
+  for (i = 0; i < argc; i++)
+  {
+    if (strcmp(argv[i], "-h") == 0)
+    {
+      printf("Opções de execução:\n\n-h --- help\n-o --- redirecionamento de saída\n");
+      printf("-f --- indicar arquivo de entrada\n-s --- saída ordenada crescente\n");
+      printf("-i --- indicar o vértice inicial\n-l --- indicar o vértice final\n");
+    }
 
-  for(int i=0; i < edges; i++) {
-    cin >> source >> destiny >> cost;
-    graph[source].push_back(make_pair(cost, destiny));
-    graph[destiny].push_back(make_pair(cost, source));
+    if (strcmp(argv[i], "-o") == 0)
+    {
+      printf("%s\n", argv[i]);
+    }
+
+    if (strcmp(argv[i], "-f") == 0)
+    {
+      ifstream input_file("input.txt", ios::in);
+
+      lli vertex, edges;
+      cin >> vertex >> edges;
+
+      distances.resize(vertex + 1, MAX_INT);
+      graph.resize(vertex + 1);
+
+      lli source, destiny, cost;
+
+      while (!input_file.eof())
+      {
+        for (int i = 0; i < edges; i++)
+        {
+          cin >> source >> destiny >> cost;
+          graph[source].push_back(make_pair(cost, destiny));
+          graph[destiny].push_back(make_pair(cost, source));
+        }
+      }
+      input_file.close();
+    }
+
+    if (strcmp(argv[i], "-s") == 0)
+    {
+      printf("%s\n", argv[i]);
+    }
+
+    if (strcmp(argv[i], "-i") == 0)
+    {
+      printf("%s\n", argv[i]);
+    }
+
+    if (strcmp(argv[i], "-l") == 0)
+    {
+      printf("%s\n", argv[i]);
+    }
   }
+
+  // END
 
   cout << bellman_ford(1) << endl;
   return 0;
